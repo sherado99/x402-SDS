9a9b982aa44e5e55790ab1d2a25321b57c2ce1b9
Stech <sheradogilang@gmail.com>
2026-05-26 12:50:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-```javascript
 import { Actor } from 'apify';
 import { CheerioCrawler } from 'crawlee';
 import got from 'got';
@@ -631,4 +630,4 @@ const output = finalResults.map(row => ({ ...row, download_docx: docxUrl, downlo
 await Actor.pushData(output);
 console.log(`Scan complete. ${output.length} endpoints found.`);
 await Actor.exit();
-```
+
