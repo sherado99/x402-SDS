d3f5c61635b9b93051349bde233f9b9c26e9c794
Stech <sheradogilang@gmail.com>
2026-05-28 16:30:15 +0900

    Update proxy.js

--- src/utils/proxy.js
+++ src/utils/proxy.js
@@ -1,14 +1,13 @@
 // src/utils/proxy.js
 import { Actor } from 'apify';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 
 export function getProxyAgent(useResidential = false) {
   try {
-    // Apify residential proxy tersedia otomatis di platform
     const proxyUrl = process.env.APIFY_PROXY_PASSWORD
       ? `http://auto:${process.env.APIFY_PROXY_PASSWORD}@proxy.apify.com:8000`
       : null;
-    if (proxyUrl ) {
-      const { HttpsProxyAgent } = require('https-proxy-agent' );
+    if (proxyUrl) {
       return new HttpsProxyAgent(proxyUrl);
     }
   } catch (e) { /* tidak tersedia, lanjut tanpa proxy */ }
